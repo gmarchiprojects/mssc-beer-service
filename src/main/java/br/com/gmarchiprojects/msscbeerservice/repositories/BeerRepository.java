d3712a0f1e96883c5413b2e9b9cc7f9dacadf116
Giovanni Marchi <giovanni.marchi92@gmail.com>
2023-01-04 13:43:36 -0300

    bootstrap ok close #6

--- src/main/java/br/com/gmarchiprojects/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/br/com/gmarchiprojects/msscbeerservice/repositories/BeerRepository.java
@@ -1,9 +1,11 @@
 package br.com.gmarchiprojects.msscbeerservice.repositories;
 
 import br.com.gmarchiprojects.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> ,
+                                        CrudRepository<Beer, UUID> {
 }
