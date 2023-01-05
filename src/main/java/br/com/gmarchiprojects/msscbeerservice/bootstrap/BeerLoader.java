770ad2724a2572ec199c5273577bba2ce161e49a
Giovanni Marchi <giovanni.marchi92@gmail.com>
2023-01-05 18:02:40 -0300

    upc long to upc string

--- src/main/java/br/com/gmarchiprojects/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/br/com/gmarchiprojects/msscbeerservice/bootstrap/BeerLoader.java
@@ -10,6 +10,10 @@ import java.math.BigDecimal;
 @Component
 public class BeerLoader implements CommandLineRunner {
 
+    public static final String BEER_1_UPC = "01268532145632";
+    public static final String BEER_2_UPC = "01225145445632";
+    public static final String BEER_3_UPC = "36975208145632";
+
     private final BeerRepository beerRepository;
 
     public BeerLoader(BeerRepository beerRepository) {
@@ -29,7 +33,7 @@ public class BeerLoader implements CommandLineRunner {
                     .beerStyle("IPA")
                     .quantityToBrew(200)
                     .minOnHand(12)
-                    .upc(123456789L)
+                    .upc(BEER_1_UPC)
                     .price(new BigDecimal("12.95"))
                     .build());
 
@@ -38,7 +42,16 @@ public class BeerLoader implements CommandLineRunner {
                     .beerStyle("PALE_ALE")
                     .quantityToBrew(200)
                     .minOnHand(12)
-                    .upc(123456781L)
+                    .upc(BEER_2_UPC)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Budweiser")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(BEER_3_UPC)
                     .price(new BigDecimal("11.95"))
                     .build());
         }
--- src/test/java/br/com/gmarchiprojects/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/br/com/gmarchiprojects/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,18 +1,24 @@
 package br.com.gmarchiprojects.msscbeerservice.web.controller;
 
+import br.com.gmarchiprojects.msscbeerservice.bootstrap.BeerLoader;
+import br.com.gmarchiprojects.msscbeerservice.domain.Beer;
 import br.com.gmarchiprojects.msscbeerservice.web.model.BeerDto;
 import br.com.gmarchiprojects.msscbeerservice.web.model.BeerStyleEnum;
+import br.com.gmarchiprojects.msscbeerservice.web.services.BeerService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @WebMvcTest(BeerController.class)
@@ -22,8 +28,13 @@ class BeerControllerTest {
     MockMvc mockMvc;
     @Autowired
     ObjectMapper objectMapper;
+
+    @MockBean
+    BeerService beerService;
     @Test
     void getBeerById() throws Exception{
+
+        given(beerService.getById(any())).willReturn(getBeerDto());
 
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString())
                 .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
@@ -31,6 +42,8 @@ class BeerControllerTest {
 
     @Test
     void saveNewBeer() throws Exception {
+
+        given(beerService.saveNewBeer(any())).willReturn(getBeerDto());
 
         BeerDto beerDto = getBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
@@ -44,6 +57,8 @@ class BeerControllerTest {
     @Test
     void updateBeerById() throws Exception{
 
+        given(beerService.updateBeer(any(), any())).willReturn(getBeerDto());
+
         BeerDto beerDto = getBeerDto();
         String beerDtotoJson = objectMapper.writeValueAsString(beerDto);
 
@@ -55,10 +70,10 @@ class BeerControllerTest {
 
     BeerDto getBeerDto(){
         return BeerDto.builder()
-                .beerName("Beer")
-                .beerStyle(BeerStyleEnum.IPA)
-                .upc(123456L)
-                .price(new BigDecimal("12.55"))
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyleEnum.PALE_ALE)
+                .upc(BeerLoader.BEER_1_UPC)
+                .price(new BigDecimal("11.95"))
                 .build();
     }
 }
